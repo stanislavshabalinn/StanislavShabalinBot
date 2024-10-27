39487780943d93a0a82fe9d34278088fa2dedf12
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-10-27 19:54:53 +0700

    Controller

---
+++ Controllers/InlineKeyboardController.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace VoiceTexterBot.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramClient = telegramBotClient;
+        }
+        // InlineKeyboardController.cs
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+        }
+    }
+}
---
+++ Controllers/VoiceMessageController.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace VoiceTexterBot.Controllers
+{
+    public class VoiceMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public VoiceMessageController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramClient = telegramBotClient;
+        }
+        // VoiceMessageController.cs
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} получил сообщение");
+            await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"Получено голосовое сообщение", cancellationToken: ct);
+        }
+    }
+}
